aa8765d13b69d29517bcb71d85ee14bf434384f7
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-22 16:48:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggit::Application.config.secret_token = '538359da50dcde219812ccdbbf2ab391bdec91480cc55f7d4b6e0f3b30c8016ebf4d41abe89de51abd8051878baa05b702296986ea66f506971a0f7eb96a3be7'
