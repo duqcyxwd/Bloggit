bf0aa70ef3b8398225ffd64321909dece30c02ca
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-23 17:17:10 -0400

    Created welcome controller with index and about views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Bloggit::Application.routes.draw do
 
   get "welcome/about"
 
+  root to: 'welcome#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
