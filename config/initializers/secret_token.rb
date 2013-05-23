bcfd28e101cd32efc5a5e600a7ed6f99a1af89b4
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-22 21:36:51 -0400

    Create readme file set up database, postgreSQL

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bloggit::Application.config.secret_token = '538359da50dcde219812ccdbbf2ab391bdec91480cc55f7d4b6e0f3b30c8016ebf4d41abe89de51abd8051878baa05b702296986ea66f506971a0f7eb96a3be7'
+Bloggit::Application.config.secret_token = '6186bad0df83bf2e61a323bc1c3bbaf76781b860a39345b908f0af878372c9a1612d2a6b8d0add86fbdee6eb7e12c534f03f3d7dfab6b557fc193763fdd16057'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bloggit::Application.config.session_store :cookie_store, key: '_Bloggit_session'
+Bloggit::Application.config.session_store :cookie_store, key: '_bloggit_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
