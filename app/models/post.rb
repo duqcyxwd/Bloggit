09e139c0c75ad65d7bbb288908edef0a99f59e3e
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-24 16:57:45 -0400

    Installed Faker and seeded the database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
 	has_many :comments
-	attr_accessible :body, :title
+  attr_accessible :body, :title
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+rand(10..30).times do
+  p = Post.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+  rand(3..10).times do
+    p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  end
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
