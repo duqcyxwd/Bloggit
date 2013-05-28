3d1de3b832a26c0e9d627160172191e5c6cf9ae9
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-05-27 20:35:28 -0400

    temp commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Bloggit::Application.routes.draw do
   
   resources :posts
 
+
+  get "welcome/index"
+  get "welcome/about"
+
   match "about" => 'welcome#about', via: :get
 
-  # root :to => 'welcome#index'
-  root to: 'welcome#index'
+  root :to => 'welcome#index'  #Pass as a hash :to is key, 
+  # root to: 'welcome#index'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
