474bbbf7c292ccb7e0dd0bd0f668b86bcea0df3c
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-12 18:36:38 -0400

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,10 +59,10 @@ end
 
 u = User.find(1)
 u.update_attributes(name:'Super Smart', email: 'duqcyxwd@gmail.com', password: '19095750', password_confirmation: '19095750')
-# u.confirm!
-u.skip_confirmation!
-u.save
-u.update_attribute(:role, 'admin')
+u.confirm!
+# u.skip_confirmation!
+# u.save
+# u.update_attribute(:role, 'admin')
 
 u = User.new(
   name: 'Admin User',
