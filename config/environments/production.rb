3fa5a8882b225e8c1c2d822d0c2d445d7c8ddac3
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-13 00:40:09 -0400

    Change mailer_sender to bloggit.du@gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Bloggit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { :host => 'bloccit.herokuapp.com' }  
 end
