b4fa7706defc158c3da39ca74a6d13dc8f63e341
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-17 17:59:34 -0400

    Installed and configured pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,5 +37,16 @@ module ApplicationHelper
 	# 	redcarpet = Redcarpet::Markdown.new(renderer, extensions)
 	# 	(redcarpet.render text).html_safe
 	# end
-end
 
+	# change the default link renderer for will_paginate
+	def will_paginate(collection_or_options = nil, options = {})
+		if collection_or_options.is_a? Hash
+			options, collection_or_options = collection_or_options, nil
+		end
+		unless options[:renderer]
+			options = options.merge :renderer => BootstrapLinkRenderer
+		end
+		super *[collection_or_options, options].compact
+	end
+
+end
