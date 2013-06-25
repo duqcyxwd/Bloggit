ca07d2a168f733e80bc9399ef27247dbcaf67353
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-25 18:37:35 -0400

    Implemented Fackbook Authetication

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,23 @@ class User < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader # add this line.
 
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      pass = Devise.friendly_token[0,20]
+      user = User.new(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password: pass,
+                         password_confirmation: pass
+                        )
+      user.skip_confirmation!
+      user.save
+    end
+    user
+  end
+
   # attr_accessible :title, :body
   ROLES = %w[member moderator admin]
   def role?(base_role)
