2440e138e159f155abf1f3e15dad366b3c876cc7
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-26 16:47:59 -0400

    add markdown to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,10 +11,10 @@ class Comment < ActiveRecord::Base
 
 	def send_favorite_emails
 		self.post.favorites.each do |favorite|
-			# if fav.user_id != self.user_id && fav.user.email_favorites?
+			if favorite.user_id != self.user_id && favorite.user.email_favorites?
 				FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
 				##deliver to where??????
-			# end
+			end
 		end
 	end
 end
