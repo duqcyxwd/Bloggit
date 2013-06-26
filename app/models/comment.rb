bd97b52ecc3424e32ce5c704a239efad3f489774
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-26 13:52:23 -0400

    Implemented favoriting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,19 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :user
-  attr_accessible :body, :post, :user
+	belongs_to :post
+	belongs_to :user
+	attr_accessible :body, :post, :user
 
-  validates :body, length: { minimum: 5 }, presence: true
-  validates :user, presence: true
+	validates :body, length: { minimum: 5 }, presence: true
+	validates :user, presence: true
+
+	after_create :send_favorite_emails
+	private
+
+	def send_favorite_emails
+		self.post.favorites.each do |favorite|
+			# if fav.user_id != self.user_id && fav.user.email_favorites?
+			  FavoriteMailer.new_comment(fav.user, self.post, self).deliver
+			# end
+		end
+	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Bloggit::Application.configure do
 
   #Configure for device
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'your-app-name.dev' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
