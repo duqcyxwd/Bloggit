cf64fe7c97705bd1d8356add3657b537e6f14a91
Yongqinchuan Du <duqcyxwd@gmail.com>
2013-06-27 11:20:15 -0400

    small change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource)
-		session[:user_return_to] || root_path
+		session[:user_return_to] || topics_path || root_path
 	end
 
 	def after_sign_out_path_for(resource_or_scope)
